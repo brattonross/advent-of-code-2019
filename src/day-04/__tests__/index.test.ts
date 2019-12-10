2070cf473673e7d04524af349a37d6f17d71223d
Ross Bratton <bratton.ross@gmail.com>
2019-12-10 22:28:13 +0000

    Day 04 part 2

--- src/day-04/__tests__/index.test.ts
+++ src/day-04/__tests__/index.test.ts
@@ -2,12 +2,12 @@ import { hasTwoAdjacentSame, increases } from '..'
 
 test('hasTwoAdjacentSame', () => {
   const cases = [
-    { num: 11111, expected: true },
-    { num: 123456789, expected: false },
-    { num: 122345, expected: true },
-    { num: 123345, expected: true },
-    { num: 123445, expected: true },
-    { num: 123455, expected: true }
+    { num: [1, 1, 1, 1, 1], expected: true },
+    { num: [1, 2, 3, 4, 5, 6, 7, 8, 9], expected: false },
+    { num: [1, 2, 2, 3, 4, 5], expected: true },
+    { num: [1, 2, 3, 3, 4, 5], expected: true },
+    { num: [1, 2, 3, 4, 4, 5], expected: true },
+    { num: [1, 2, 3, 4, 5, 5], expected: true }
   ]
 
   for (const tc of cases) {
@@ -19,9 +19,9 @@ test('hasTwoAdjacentSame', () => {
 
 test('increases', () => {
   const cases = [
-    { num: 11110, expected: false },
-    { num: 123456789, expected: true },
-    { num: 223450, expected: false }
+    { num: [1, 1, 1, 1, 0], expected: false },
+    { num: [1, 2, 3, 4, 5, 6, 7, 8, 9], expected: true },
+    { num: [2, 2, 3, 4, 5, 0], expected: false }
   ]
 
   for (const tc of cases) {
--- src/day-04/index.ts
+++ src/day-04/index.ts
@@ -4,21 +4,28 @@ const input = '183564-657474'
 
 const [start, end] = input.split('-').map(n => parseInt(n))
 
-export const hasTwoAdjacentSame = (n: number) =>
-  n
+/**
+ * Returns the number of occurrences of the given number in the array.
+ */
+const countOf = (ns: number[], n: number): number => {
+  return ns.reduce((acc, cur) => acc + (cur === n ? 1 : 0), 0)
+}
+
+// Since we can guarantee that the numbers increase in value, we know for
+// certain that any numbers of the same value are adjacent.
+export const hasTwoAdjacentSame = (ns: number[]): boolean =>
+  [...new Set(ns)].some(n => countOf(ns, n) === 2)
+
+export const increases = (ns: number[]): boolean =>
+  ns.every((n, i, arr) => arr[i + 1] === undefined || n <= arr[i + 1])
+
+const num = range(start, end).reduce((acc, n) => {
+  const digits = n
     .toString()
     .split('')
-    .some((n, i, arr) => arr[i + 1] === n)
+    .map(Number)
 
-export const increases = (n: number) =>
-  n
-    .toString()
-    .split('')
-    .every((n, i, arr) => arr[i + 1] === undefined || n <= arr[i + 1])
-
-const num = range(start, end).reduce(
-  (acc, n) => acc + (hasTwoAdjacentSame(n) && increases(n) ? 1 : 0),
-  0
-)
+  return acc + (increases(digits) && hasTwoAdjacentSame(digits) ? 1 : 0)
+}, 0)
 
 console.log(num)
