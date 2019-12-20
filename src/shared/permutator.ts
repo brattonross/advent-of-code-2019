2c3f3fc4e0937b2f7d500946343785d649b00ddf
Ross Bratton <bratton.ross@gmail.com>
2019-12-20 23:46:26 +0000

    Comment permutator function

--- src/shared/permutator.ts
+++ src/shared/permutator.ts
@@ -1,13 +1,16 @@
+/**
+ * Produces an array containing all permutations of the given input array.
+ */
 export default function permutator<T>(input: T[]): T[][] {
-  let result: T[][] = []
+  const result: T[][] = []
 
-  const permute = (arr: T[], m: T[] = []) => {
+  function permute(arr: T[], m: T[] = []) {
     if (arr.length === 0) {
       result.push(m)
     } else {
       for (let i = 0; i < arr.length; i++) {
-        let curr = arr.slice()
-        let next = curr.splice(i, 1)
+        const curr = arr.slice()
+        const next = curr.splice(i, 1)
         permute(curr.slice(), m.concat(next))
       }
     }
