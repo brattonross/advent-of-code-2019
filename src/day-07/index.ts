3decbad87c10b90842028bde4e13a260488b082e
Ross Bratton <bratton.ross@gmail.com>
2019-12-21 23:15:32 +0000

    Start day 07 part 2

--- src/day-07/index.ts
+++ src/day-07/index.ts
@@ -4,20 +4,32 @@ import permutator from '../shared/permutator'
 import readInput from '../shared/readInput'
 import range from '../shared/range'
 
-function runAmplifier(phaseSetting: number, input: number, code: number[]) {
-  const [value] = runIntcode(code, [phaseSetting, input])
-  return value
+class Amplifier {
+  private code: number[]
+
+  constructor(code: number[], private phaseSetting: number) {
+    this.code = [...code]
+  }
+
+  run(input: number[]): number[] {
+    return runIntcode(this.code, [this.phaseSetting, ...input])
+  }
 }
 
+/**
+ * Run the given code for each phase setting, summing the outputs for each.
+ */
 export function thruster(
   phaseSettings: number[],
   input: number,
   code: number[]
 ): number {
-  return phaseSettings.reduce(
-    (thrust, setting) => runAmplifier(setting, thrust, [...code]),
-    input
-  )
+  return phaseSettings
+    .map(s => new Amplifier(code, s))
+    .reduce((thrust, amp) => {
+      const [newTotal] = amp.run([thrust])
+      return newTotal
+    }, input)
 }
 
 const code = readInput(path.resolve(__dirname, './input.txt'))
@@ -26,8 +38,6 @@ const code = readInput(path.resolve(__dirname, './input.txt'))
 
 const possibleSettings = permutator<number>(range(0, 4))
 
-const max = Math.max(
-  ...possibleSettings.map(settings => thruster(settings, 0, code))
-)
-
-console.log(max)
+// const max = Math.max(
+//   ...possibleSettings.map(settings => thruster(settings, 0, code))
+// )
