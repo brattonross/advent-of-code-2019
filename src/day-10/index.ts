2ca81a349c8aee157b6959dac640e8370595c80a
Ross Bratton <bratton.ross@gmail.com>
2019-12-27 00:04:55 +0000

    Day 10 part 2

--- src/day-10/index.ts
+++ src/day-10/index.ts
@@ -43,24 +43,38 @@ export function asteroids(map: string[][]) {
 }
 
 /**
+ * Creates a map of angle to asteroids (excluding the station location).
+ */
+function asteroidMap(asteroids: Point[], station: Point) {
+  return asteroids.reduce((map, asteroid) => {
+    if (asteroid.x === station.x && asteroid.y === station.y) {
+      return map
+    }
+
+    const a = angle(station, asteroid)
+    if (!map.has(a)) {
+      map.set(a, [])
+    }
+    const entry = map.get(a)
+    entry!.push(asteroid)
+
+    return map
+  }, new Map<number, Point[]>())
+}
+
+/**
  * Returns the number of visible asteroids for the asteroid at
  * the given co-ordinates.
  */
 export function visibleAsteroids(asteroids: Point[], pos: Point): number {
-  return asteroids.reduce((set, asteroid) => {
-    if (asteroid.x === pos.x && asteroid.y === pos.y) {
-      return set
-    }
-
-    return set.add(angle(pos, asteroid))
-  }, new Set<number>()).size
+  return asteroidMap(asteroids, pos).size
 }
 
 /**
  * Returns the angle between the positive x-axis and the line created by the points a and b.
  */
 function angle(a: Point, b: Point): number {
-  return Math.atan2(a.y - b.y, a.x - b.x)
+  return Math.atan2(a.y - b.y, a.x - b.x) * (180 / Math.PI)
 }
 
 const map = readInput(path.resolve(__dirname, 'input.txt'))
@@ -73,3 +87,35 @@ function part1() {
 
 const ans1 = part1()
 console.log('Part 1:', ans1)
+
+function part2() {
+  const as = asteroids(map)
+  const station = bestAsteroid(map)
+  const am = asteroidMap(as, station!.point)
+
+  // Sort the map so that we iterate over the asteroids in a "clockwise" order.
+  const sortedMap = [...am.entries()].sort((a, b) => a[0] - b[0])
+
+  // We want to start from 90deg because this is "up" in terms of an angle from the x-axis.
+  const startPos = sortedMap.findIndex(e => e[0] === 90)
+
+  let totalDestroyed = 0
+  const points = sortedMap.map(e => e[1])
+
+  // Continuously iterate over the array in order, removing the first
+  // asteroid found at each angle (if there is one), and adding to the
+  // destroyed count until we reach 200.
+  for (let i = startPos; ; i = (i + 1) % points.length) {
+    if (!points[i].length) {
+      continue
+    }
+
+    const destroyed = points[i].shift()!
+    if (++totalDestroyed === 200) {
+      return destroyed.x * 100 + destroyed.y
+    }
+  }
+}
+
+const ans2 = part2()
+console.log('Part 2:', ans2)
